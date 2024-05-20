3b8297222ad236331fc53e8ffd44a6a0a9d38bc3
ExoXelo <Lexa02125@mail.ru>
2024-05-20 16:27:01 +0300

    ltest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher, types, executor
 from config import TELEGRAM_TOKEN
+from keyboard.keybaords import get_keyboard_1, get_keyboard_2
 
 bot = Bot(token= TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
@@ -8,32 +9,31 @@ async def set_commands(bot: Bot):
     commands = [
         types.BotCommand(command='/start', description= 'Запуск бота'),
         types.BotCommand(command='/help', description='Help'),
-        types.BotCommand(command='/one', description='one'),
-        types.BotCommand(command='/two', description='two'),
-        types.BotCommand(command='/three', description='three')
     ]
 
     await bot.set_my_commands(commands)
 
 @dp.message_handler(commands= 'start')
 async def start(message: types.Message):
-    await message.answer('эхо бот')
+    await message.answer('эхо бот', reply_markup= get_keyboard_1())
 
-@dp.message_handler(commands='help')
-async def help(message: types.Message):
-    await message.answer('help')
+@dp.message_handler(lambda message: message.text == 'Отправь фото кота')
+async def button_1_click(message: types.Message):
+    await bot.send_photo(message.chat.id, photo= 'https://img.razrisyika.ru/kart/132/1200/526072-tolstyy-kotik-26.jpg', caption= 'ля какой')
 
-@dp.message_handler(commands='one')
-async def one(message: types.Message):
-    await message.answer('one')
+@dp.message_handler(lambda message: message.text == 'Перейти на некст клаву')
+async def button_2_click(message: types.Message):
+    await message.answer('Перейти на некст клаву', reply_markup= get_keyboard_2())
 
-@dp.message_handler(commands='two')
-async def two(message: types.Message):
-    await message.answer('two')
+@dp.message_handler(lambda message: message.text == 'Отправь фото собаки')
+async def button_3_click(message: types.Message):
+    await bot.send_photo(message.chat.id, photo= 'https://www.nairaland.com/attachments/14131018_images65_jpeg_jpeg4a48023df950c430bd5f8313a4f51b09', caption= 'Прикол')
 
-@dp.message_handler(commands='three')
-async def three(message: types.Message):
-    await message.answer('three')
+@dp.message_handler(lambda message: message.text == 'На прошлую клаву')
+async def button_2_click(message: types.Message):
+    await message.answer('На прошлую клаву', reply_markup= get_keyboard_1())
+async def start(message: types.Message):
+    await message.answer('эхо бот', reply_markup= get_keyboard_1())
 
 @dp.message_handler()
 async def echo(message: types.Message):
